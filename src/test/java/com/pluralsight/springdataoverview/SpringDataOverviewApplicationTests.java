9680432c6dce7c353a2dfaff0d84eee6c7e228b5
ben.strube <benjamin.strube@ust.com>
2022-01-04 10:58:50 -0500

    updated main test class

--- src/test/java/com/pluralsight/springdataoverview/SpringDataOverviewApplicationTests.java
+++ src/test/java/com/pluralsight/springdataoverview/SpringDataOverviewApplicationTests.java
@@ -1,13 +1,44 @@
 package com.pluralsight.springdataoverview;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import java.time.LocalDateTime;
+import java.util.List;
 
-@SpringBootTest
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import com.pluralsight.springdataoverview.entity.Flight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@DataJpaTest
 class SpringDataOverviewApplicationTests {
 
+	@Autowired
+	private EntityManager entityManager;
+
 	@Test
-	void contextLoads() {
+	public void verifyFlightCanBeSaved() {
+		final Flight flight = new Flight();
+		flight.setOrigin("Amsterdam"); 
+		flight.setDestination("New york");
+		flight.setScheduledAt(LocalDateTime.parse("2011-12-13T12:12:00"));
+
+		entityManager.persist(flight);
+
+		final TypedQuery<Flight> results = entityManager
+			.createQuery("SELECT f FROM Flight f", Flight.class);
+
+			final List<Flight> resultList = results.getResultList();
+
+			assertThat(resultList)
+				.hasSize(1)
+				.first()
+				.isEqualTo(flight);
 	}
 
 }
